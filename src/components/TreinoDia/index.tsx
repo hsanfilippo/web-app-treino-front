3bb97f6655104b9612578ae7e3785069485a1a0c
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-09-14 20:48:53 +0900

    Feat: Criação do componente que mostra o treino do dia

--- src/components/TreinoDia/index.tsx
+++ src/components/TreinoDia/index.tsx
@@ -1,8 +1,26 @@
+import * as S from './styles'
+
 const TreinoDia = () => {
 
   return (
     <>
-      <h1>aaaa</h1>
+      <S.Container>
+        <S.MediumTitle>Seu treino de hoje</S.MediumTitle>
+        <S.Card>
+          <S.CardTitle>Dia de Peito</S.CardTitle>
+          <S.Lista>
+            <S.ItemLista>Supino inclinado halter</S.ItemLista>
+            <S.ItemLista>Supino reto halter</S.ItemLista>
+            <S.ItemLista>Supino inclinado maq.</S.ItemLista>
+            <S.ItemLista>Flexao de braco</S.ItemLista>
+            <S.ItemLista>Esteira 6.5km/h - 20min</S.ItemLista>
+          </S.Lista>
+          <S.ButtonContainer>
+            <S.ButtonSecondary>Ver mais</S.ButtonSecondary>
+            <S.ButtonPrimary>Comecar</S.ButtonPrimary>
+          </S.ButtonContainer>
+        </S.Card>
+      </S.Container>
     </>
 
   )
