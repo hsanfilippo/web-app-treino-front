82d39c7fa8f267dfa9420261c747710a860eb02a
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-09-19 18:33:16 +0900

    Feat: Component que adiciona um novo treino, via requisição POST ao backend (porta 8000)

--- src/components/FormNewTreino/index.tsx
+++ src/components/FormNewTreino/index.tsx
@@ -1,20 +1,84 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import * as S from '../../styles/index'
 import * as Styles from './styles'
 
 const FormNewTreino = () => {
+  const [error, setError] = useState<string | null>(null)
+  const [nome, SetNome] = useState<string>('')
+  const [descricao, SetDescricao] = useState<string>('')
+
+  const navigate = useNavigate()
+
+  const postTreino = async (nome: string, descricao: string) => {
+    try {
+      const novoTreino = {
+        nome,
+        descricao
+      }
+
+      const response = await fetch('http://localhost:8000/api/treinos/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(novoTreino)
+      })
+      if (!response.ok) {
+        throw new Error('Erro ao requisitar os treinos da API')
+      }
+      const data = await response.json()
+    } catch (err: any) {
+      setError(err.message)
+    }
+  }
+
+  function validateFields(inputNome: string, inputDesc: string) {
+    if (inputNome.length === 0) {
+      alert('Campo "Nome do treino" está vazio.')
+      return false
+    } else if (inputDesc.length === 0) {
+      alert('Campo "Descrição" está vazio.')
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (validateFields(nome, descricao)) {
+      postTreino(nome, descricao)
+      alert('Seu treino foi criado! ([POST]localhost:8000/api/treinos/)')
+      navigate('/treinos/')
+    }
+  }
+
+  if (error) {
+    return <S.Container>Msg de erro: {error}</S.Container>
+  }
+
   return (
     <>
       <S.Container>
         <S.Card>
-          <Styles.FormContainer>
+          <Styles.FormContainer onSubmit={handleSubmit}>
             <input
               type="text"
-              name="nome_do_treino"
-              placeholder="nome do treino"
+              value={nome}
+              onChange={(e) => SetNome(e.target.value)}
+              placeholder="Nome do treino"
             />
-            <textarea placeholder="Descrição" />
+            <textarea
+              value={descricao}
+              onChange={(e) => SetDescricao(e.target.value)}
+              placeholder="Descrição"
+            />
+            <a href="http://localhost:3000/treinos/">
+              <S.LgButtonSecondary type="submit">Criar</S.LgButtonSecondary>
+            </a>
           </Styles.FormContainer>
-          <S.LgButtonSecondary>Criar</S.LgButtonSecondary>
         </S.Card>
       </S.Container>
     </>
