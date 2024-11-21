b14be60c4e1caa4dfb9a8a12170b3e47d3d9f88c
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2024-11-21 20:47:35 +0900

    Fix: Botões de Edit e Delete ajustados para mobile

--- src/components/ListTreinos/styles.ts
+++ src/components/ListTreinos/styles.ts
@@ -1,3 +1,5 @@
+import { cores } from '../../styles'
+
 import styled from 'styled-components'
 
 export const DescTreinoContainer = styled.div`
@@ -14,6 +16,24 @@ export const TreinoInfoContainer = styled.div`
 
 export const ResumoTreino = styled.div`
   background-color: red;
+`
+
+export const EditDeleteDiv = styled.div`
+  display: inline;
+`
+
+export const ButtonEditDelete = styled.button`
+  background-color: transparent;
+  color: ${cores.textoPrimario};
+  height: 40px;
+  width: 70px;
+  padding: 10px;
+  font-weight: bold;
+  font-size: 14px;
+  border-radius: 8px;
+  border: 2px solid ${cores.corSecundaria};
+  cursor: pointer;
+  margin-right: 8px;
 `
 
 export const EditadoEm = styled.div`
